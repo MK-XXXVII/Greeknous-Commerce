404caf21201edec38cf370e9693d0cb16e5d96c2
Michail Kalogeras <mike@cutegang.nl>
2021-09-25 08:59:17 +0200

    integrated firebase into application, created sign in page with sign in component and sign in with Google

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+const config = {
+    apiKey: "AIzaSyDSYFn1axHfaZdqk9erKf3A2B6SnkV5h-M",
+    authDomain: "dotstigma.firebaseapp.com",
+    projectId: "dotstigma",
+    storageBucket: "dotstigma.appspot.com",
+    messagingSenderId: "1076151588439",
+    appId: "1:1076151588439:web:0b3f0e90b4f4622b127512",
+    measurementId: "G-3ZT34FXNZW"
+  };
+
+  firebase.initializeApp(config);
+
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+  
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({ prompt: 'select_account' });
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+  
+  export default firebase;
